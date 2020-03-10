73ef8a831f5c1e6433a640a1482e5beaa91f533b
Austin Redmond <aredmond85@gmail.com>
2020-03-10 20:46:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+#Prime = number > 1
+#Prime = number whose only factor is 1 and itself
+#Prime = 1 is not prime
+def prime?(integer)
+  if integer > 1
+    (2...integer).each {|n| return false if integer < 2 || integer % n == 0}
+    return true
+  else
+    return false
+  end
+end
